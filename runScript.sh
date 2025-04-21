e2f4ed20cb1e6e78f8ad5c90bdb050d35644cdfa
Izaak White <ww655421@ohio.edu>
2025-04-21 14:17:28 -0400

    modified runscript

--- runScript.sh
+++ runScript.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Izaak White and Joshua Moner
-echo "Building tree_builder..."
 make
 
-echo "Running tree_builder parser on plisTest.txt..."
 ./tree_builder < plisTest.txt > parser_output.txt
 
-echo "Parsing complete. Results saved to parser_output.txt"
 cat parser_output.txt
 
 make clean
